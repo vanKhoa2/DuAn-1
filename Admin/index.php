1df0967071158a819f978f815fb06fa9234d46c5
toanbanrau <thieutoan2004@gmail.com>
2024-11-13 11:30:22 +0700

    suafile

--- Admin/index.php
+++ Admin/index.php
@@ -5,10 +5,10 @@ require_once './commons/env.php'; // Khai báo biến môi trường
 require_once './commons/function.php'; // Hàm hỗ trợ
 
 // Require toàn bộ file Controllers
-require_once './controllers/HomeController.php';
+require_once './controllers/SanPhamController.php';
 
 // Require toàn bộ file Models
-require_once './models/Student.php';
+require_once './models/SanPham.php';
 
 // Route
 $act = $_GET['act'] ?? '/';
